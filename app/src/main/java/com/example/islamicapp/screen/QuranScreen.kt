b7bcedfa4d412ed83da413e051fcb914f98a5f31
Umair Khan <uk236393@gmail.com>
2025-08-26 16:48:34 +0500

    quranScreen

--- app/src/main/java/com/example/islamicapp/screen/QuranScreen.kt
+++ app/src/main/java/com/example/islamicapp/screen/QuranScreen.kt
@@ -1,9 +1,105 @@
 package com.example.islamicapp.screen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 
 @Composable
 fun QuranScreen(navController: NavController) {
+    var textField by remember { mutableStateOf("") }
+    Box(modifier = Modifier.fillMaxSize()) {
+        Image(
+            painter = painterResource(id = com.example.islamicapp.R.drawable.background),
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
+        )
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(id = com.example.islamicapp.R.drawable.masjid),
+                contentDescription = null,
+                modifier = Modifier.size(210.dp),
+                contentScale = ContentScale.Crop
+            )
+            CustomStyledTextField(
+                value = textField,
+                onValueChange = { textField = it }
+            )
 
+
+        }
+
+    }
+
+}
+
+@Composable
+fun CustomStyledTextField(
+    value: String,
+    onValueChange: (String) -> Unit
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth(0.9f)
+            .background(Color.Transparent)
+            .clip(RoundedCornerShape(10.dp))
+            .border(1.dp, Color(0xFFDAC089), RoundedCornerShape(10.dp))
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(start = 8.dp)
+        ) {
+            Icon(
+                painter = painterResource(id = com.example.islamicapp.R.drawable.quran1),
+                contentDescription = "Islamic Icon",
+                tint = Color(0xFFDAC089),
+                modifier = Modifier.size(24.dp)
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            TextField(
+                value = value,
+                onValueChange = onValueChange,
+                placeholder = { Text("Sura Name", color = Color.Gray, fontSize = 14.sp) },
+                colors = TextFieldDefaults.textFieldColors(
+                    backgroundColor = Color.Transparent,
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent,
+                    textColor = Color.White
+                ),
+                modifier = Modifier.weight(1f)
+            )
+
+        }
+    }
 }
